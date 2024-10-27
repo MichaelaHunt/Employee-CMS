e439b7f6247120fafe69d3b4014ab6f5271d678b
Michaela Hunt <mhunt@theprovatogroup.com>
2024-10-27 13:22:19 -0400

    Completed seeds script

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO (department)
+VALUES ('Accounting'),
+('HR'),
+('R&D'),
+('Marketing');
+
+INSERT INTO (role)
+VALUES ('Graphic Designer', 50000, 4),
+('Accountant', 60000, 1),
+('Researcher', 70000, 2);
+
+INSERT INTO (employee)
+VALUES ('Sarah', 'Powers', 1, NULL),
+('Matthew', 'Johnson', 2, 1);
