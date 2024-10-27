044edf96d15d7a1075cb9c4e01ce8ff32de65cf5
Michaela Hunt <mhunt@theprovatogroup.com>
2024-10-27 15:00:09 -0400

    made changes to the sql scripts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO (department)
+INSERT INTO department (name)
 VALUES ('Accounting'),
 ('HR'),
 ('R&D'),
 ('Marketing');
 
-INSERT INTO (role)
+INSERT INTO role (title, salary, department_id)
 VALUES ('Graphic Designer', 50000, 4),
 ('Accountant', 60000, 1),
 ('Researcher', 70000, 2);
 
-INSERT INTO (employee)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Sarah', 'Powers', 1, NULL),
 ('Matthew', 'Johnson', 2, 1);
--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ function performActions(response: any) {
             break;
         case "Add a department":
             //do an inquirer to get the info you need
+            handleRoleAddition();
             //then do an add
             break;
         case "Add a role":
@@ -64,27 +65,28 @@ function handleRoleAddition() {//need title, salary, and department_id
     //grab the current departments
     let rows = dbService.GetDepartmentTable();//I'm aware I can just do a "SELECT name FROM department" but I don't feel like it
     //format them into an array of just the names
-    let list = rows.map((item) => item.name);
-    list.push("None");
-    //ask de questions
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "Please enter the title of the new role.",
-            name: 'title'
-        }, 
-        {
-            type: 'input',
-            message: "Please enter the salary of the new role.",
-            name: 'salary'
-        },
-        {
-            type: 'list',
-            message: "Please select the department this role will belong to.",
-            name: 'department',
-            options: list
-        }
-    ]).then();
+    console.log(rows);
+    // let list = rows.map((item) => item.name);
+    // list.push("None");
+    // //ask de questions
+    // inquirer.prompt([
+    //     {
+    //         type: 'input',
+    //         message: "Please enter the title of the new role.",
+    //         name: 'title'
+    //     }, 
+    //     {
+    //         type: 'input',
+    //         message: "Please enter the salary of the new role.",
+    //         name: 'salary'
+    //     },
+    //     {
+    //         type: 'list',
+    //         message: "Please select the department this role will belong to.",
+    //         name: 'department',
+    //         options: list
+    //     }
+    // ]).then();
 }
 
 function handleEmployeeAddition() {//need first name, last name, role_id, and manager_id.
