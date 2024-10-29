1228ab6a32779efe5bd23157753cce20e4c8a129
Michaela Hunt <mhunt@theprovatogroup.com>
2024-10-29 19:53:49 -0400

    "add a ..." actions finished and tested

--- dist/dbServices.js
+++ dist/dbServices.js
@@ -1,8 +1,5 @@
-
 import { pool, connectToDb } from './connection.js';
-
 await connectToDb();
-
 class dbServices {
     // GetDepartmentTable() {
     //     let rows: any;
@@ -16,84 +13,80 @@ class dbServices {
     //     console.log(rows);
     //     return rows;
     // }
-
     async GetDepartmentTable() {
         try {
             const result = await pool.query('SELECT * FROM department');
             return result.rows;
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
             // throw err;
         }
     }
-
     async GetRoleTable() {
         try {
             const result = await pool.query('SELECT * FROM role');
             return result.rows;
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
             // throw err;
         }
     }
-
     async GetEmployeeTable() {
         try {
             const result = await pool.query('SELECT * FROM employee');
             return result.rows;
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
             // throw err;
         }
     }
-
-    async AddDepartment(name: string) {
+    async AddDepartment(name) {
         const params = [name];
         try {
             await pool.query('INSERT INTO department (name) VALUES ($1)', params, (err, _result) => {
                 if (err) {
                     console.log(err);
-                } 
+                }
             });
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
             // throw err;
         }
     }
-
-    async AddRole(title: string, salary: number, department_id: number) {
+    async AddRole(title, salary, department_id) {
         const params = [title, salary, department_id];
         try {
             await pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', params, (err, _result) => {
                 if (err) {
                     console.log(err);
-                } 
+                }
             });
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
             // throw err;
         }
     }
-
-    async AddEmployee(firstName: string, lastName: string, roleId: number, managerId: number) {
+    async AddEmployee(firstName, lastName, roleId, managerId) {
         const params = [firstName, lastName, roleId, managerId];
-
         console.log(`first name: ${firstName}, last name: ${lastName}, roleId: ${roleId}, managerId: ${managerId}`);
         try {
             await pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', params, (err, _result) => {
                 if (err) {
                     console.log(err);
-                } 
+                }
             });
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
             // throw err;
         }
     }
-
     async UpdateEmployee() {
-
     }
 }
-
 export default dbServices;
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
 import inquirer from "inquirer";
 import dbServices from './dbServices.js';
-
 const dbService = new dbServices();
-
 const actionOptions = [
     "View all departments",
     "View all roles",
@@ -13,17 +11,15 @@ const actionOptions = [
     "Update an employee's role",
     "Exit"
 ];
-
 function init() {
     inquirer.prompt([{
-        type: 'list',
-        message: "What would you like to do?",
-        name: 'action',
-        choices: actionOptions
-    }]).then((response: any) => performActions(response));
+            type: 'list',
+            message: "What would you like to do?",
+            name: 'action',
+            choices: actionOptions
+        }]).then((response) => performActions(response));
 }
-
-async function performActions(response: any) {
+async function performActions(response) {
     let rows;
     switch (response.action) {
         case "View all departments":
@@ -45,26 +41,22 @@ async function performActions(response: any) {
             await dbService.AddDepartment(name);
             break;
         case "Add a role":
-            let {title, salary, departmentId } = await handleRoleAddition();
+            let { title, salary, departmentId } = await handleRoleAddition();
             await dbService.AddRole(title, salary, departmentId);
-
             break;
         case "Add an employee":
-            let {firstName, lastName, roleId, managerId } = await handleEmployeeAddition();
+            let { firstName, lastName, roleId, managerId } = await handleEmployeeAddition();
             await dbService.AddEmployee(firstName, lastName, roleId, managerId);
             break;
         case "Update an employee's role":
-
-
             break;
     }
     if (response.action != "Exit") {
         init();
     }
 }
-
-async function handleDepartmentAddition() {//only need name
-    let name = '';//this is the issue
+async function handleDepartmentAddition() {
+    let name = ''; //this is the issue
     await inquirer.prompt([
         {
             type: 'input',
@@ -74,12 +66,11 @@ async function handleDepartmentAddition() {//only need name
     ]).then((response) => name = response.name);
     return name;
 }
-
-async function handleRoleAddition() {//need title, salary, and department_id
+async function handleRoleAddition() {
     //grab the current departments
-    let rows = await dbService.GetDepartmentTable();//I'm aware I can just do a "SELECT name FROM department" but I don't feel like it
+    let rows = await dbService.GetDepartmentTable(); //I'm aware I can just do a "SELECT name FROM department" but I don't feel like it
     //format them into an array of just the names
-    let list = rows!.map((item) => item.name as string);
+    let list = rows.map((item) => item.name);
     list.push("None");
     //ask de questions
     let departmentId = 0;
@@ -104,26 +95,23 @@ async function handleRoleAddition() {//need title, salary, and department_id
         }
     ]).then((response) => {
         console.log("Entered the then clause");
-        for (let item of rows!) {
+        for (let item of rows) {
             if (response.department === item.name) {
                 console.log("Response.department is: " + response.department);
                 departmentId = item.id;
             }
-        } 
+        }
         title = response.title;
         salary = response.salary;
     });
-
-    return {title, salary, departmentId };
+    return { title, salary, departmentId };
 }
-
-async function handleEmployeeAddition() {//need first name, last name, role_id, and manager_id.
+async function handleEmployeeAddition() {
     let roleRows = await dbService.GetRoleTable();
     let managerRows = await dbService.GetEmployeeTable();
     //format them into an array of just the names
-    let roles = roleRows!.map((item) => item.title);
-    let managers = managerRows!.map((item) => `${item.first_name} ${item.last_name}`);//undefined undefined, apparently. TODO:
-
+    let roles = roleRows.map((item) => item.title);
+    let managers = managerRows.map((item) => `${item.first_name} ${item.last_name}`); //undefined undefined, apparently. TODO:
     let firstName = '';
     let lastName = '';
     let roleId = 0;
@@ -152,20 +140,19 @@ async function handleEmployeeAddition() {//need first name, last name, role_id, 
             choices: managers
         }
     ]).then((response) => {
-        for (let item of roleRows!) {
+        for (let item of roleRows) {
             if (response.role === item.title) {
                 roleId = item.id;
             }
-        } 
-        for (let item of managerRows!) {
+        }
+        for (let item of managerRows) {
             if (response.manager === `${item.first_name} ${item.last_name}`) {
                 managerId = item.id;
             }
-        } 
+        }
         firstName = response.firstName;
         lastName = response.lastName;
     });
     return { firstName, lastName, roleId, managerId };
 }
-
 init();
